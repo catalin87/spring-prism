a0e2b86875b8aac73280b5e08e67e0fc2a5f158d
Catalin <catalin87@gmail.com>
2026-03-23 19:33:39 +0200

    chore: refresh java license headers
    Signed-off-by: Catalin <catalin87@gmail.com>

--- prism-dashboard/src/main/java/io/github/catalin87/prism/dashboard/package-info.java
+++ prism-dashboard/src/main/java/io/github/catalin87/prism/dashboard/package-info.java
@@ -1,17 +1,28 @@
 /*
- * Copyright (c) 2026 Catalin Dordea and Spring Prism Contributors
+ * Copyright (c) 2026 Catalin Dordea and Spring Prism Contributors.
+ * All rights reserved.
  *
- * Licensed under the EUPL, Version 1.2 or later (the "Licence");
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at:
+ * SPDX-License-Identifier: EUPL-1.2 OR LicenseRef-Commercial
  *
- * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ * DUAL LICENSE NOTICE:
+ *  This file is part of the "Spring Prism" project. This source code is
+ *  subject to a dual-licensing model. You may use this work under the
+ *  terms of either:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the Licence is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and
- * limitations under the Licence.
+ * 1. The European Union Public Licence (EUPL), Version 1.2 or later.
+ *  Note: The EUPL includes strong copyleft and network distribution
+ *  (SaaS) clauses. You may obtain a copy of the Licence at:
+ *  https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * 2. A Commercial Enterprise License obtained from the copyright holder.
+ *  This closed-source license is required for integration into proprietary
+ *  architectures or to waive the copyleft obligations of the EUPL.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the Licence is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the Licence for the specific language governing permissions and
+ *  limitations under the Licence.
  */
 
 /**
